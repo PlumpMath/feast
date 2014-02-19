90af612b746fb8e265a2b7befbc5add4f80a23ff
Diogo F. S. Ramos <dfsr@riseup.net>
2014-02-19 13:49:19 -0300

    Add GPL3+ and commentary banner

--- channel.scm
+++ channel.scm
@@ -1,3 +1,34 @@
+;;; channel.scm --- Data structure for threads synchronization
+
+;; Copyright (C) 2014 Diogo F. S. Ramos <dfsr@riseup.net>
+
+;; This program is free software: you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see
+;; <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Channel is a data structure for synchronization between threads.
+
+;; A thread can put or get values from a channel.  If there isn't
+;; another thread at the other end, the thread will block until other
+;; thread appears.
+
+;; Multiple threads can put and get values from a single channel but
+;; only two at a time can communicate.
+
+;;; Code:
+
 (define-module (concurrency channel)
   #:use-module (srfi srfi-9)
   #:use-module (srfi srfi-9 gnu)
