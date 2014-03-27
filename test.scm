4c52ea4d24d8bc83a6907e9d353a25417c0c49f3
Diogo F. S. Ramos <dfsr@riseup.net>
2014-03-27 02:08:01 -0300

    Convert tests to SRFI-64

--- test.scm
+++ test.scm
@@ -1,38 +1,14 @@
-#!/usr/bin/guile \
--e main -l channel.scm -s
-!#
+(use-modules (srfi srfi-64)
+             (srfi srfi-1)
+             (concurrency channel))
 
-(use-modules (concurrency channel)
-             (ice-9 format)
-             (srfi srfi-1))
+
+(test-begin "Simple Tests")
 
-;;; Testing framework taken from the book Practical Common Lisp by
-;;; Peter Seibel and translated to Guile.
-(define test-name (make-parameter '()))
+(test-assert (channel? (make-channel)))
 
-(define-syntax-rule (define-test (name args ...) exp exp* ...)
-  (define (name args ...)
-    (parameterize ((test-name (append (test-name)
-                                      (list (syntax->datum #'name)))))
-      exp exp* ...)))
-
-(define-syntax-rule (combine-results exp ...)
-  (let ((result #t))
-    (unless exp (set! result #f)) ...
-    result))
-
-(define-syntax-rule (report-result result exp)
-  (begin
-    (format #t "~[FAIL~;pass~] ... ~a: ~s~%" (if result 1 0) (test-name) 'exp)
-    result))
-
-(define-syntax-rule (check exp ...)
-  (combine-results (report-result 'exp exp) ...))
-;;; end of the testing framework
-
-(define-test (simple-tests)
-  (check (channel? (make-channel))))
-
+(test-end "Simple Tests")
+
 (define (sender-then-receiver)
   (let* ((c (make-channel))
          (s0 (call-with-new-thread (lambda () (channel-put c 42))))
@@ -69,56 +45,51 @@
          (r0 (call-with-new-thread (lambda ()
                                      (do ((i 0 (1+ i)))
                                          ((<= t i))
-                                       (when received
-                                         (set! received
-                                               (equal? v (channel-get c)))))))))
+                                       (channel-get c))))))
     (join-thread s0)
     (join-thread r0)
     (and (thread-exited? s0) (thread-exited? r0))
     received))
 
-(define-test (one-to-one)
-  (check (sender-then-receiver)
-         (receiver-then-sender)
-         (receiver-gets-value 42)
-         (receiver-gets-value "foo")
-         (receiver-gets-value (list 1 2 3))
-         (receiver-gets-value-t-times 42 42)
-         (receiver-gets-value-t-times 42 "foo")))
+(test-begin "One to One")
 
+(test-assert (sender-then-receiver))
+(test-assert (receiver-then-sender))
+(test-assert (receiver-gets-value 42))
+(test-assert (receiver-gets-value "foo"))
+(test-assert (receiver-gets-value (list 1 2 3)))
+(test-assert (receiver-gets-value-t-times 30 42))
+(test-assert (receiver-gets-value-t-times 42 "foo"))
+
+(test-end "One to One")
+
 (define (sender-then-receivers number-of-receivers v)
   (let* ((c (make-channel))
          (s0 (call-with-new-thread (lambda () (channel-put c v))))
          (receivers
           (do ((i 0 (1+ i))
-               (rs (call-with-new-thread (lambda () (channel-get c)))
+               (rs (list (call-with-new-thread (lambda () (channel-get c))))
                    (cons (call-with-new-thread (lambda () (channel-get c))) rs)))
               ((<= number-of-receivers i) rs))))
     (join-thread s0)
     (= (count thread-exited? receivers) 1)))
 
-(define (receivers-then-senders number-of-receivers v)
+(define (receivers-then-sender number-of-receivers v)
   (let* ((c (make-channel))
          (receivers
           (do ((i 0 (1+ i))
-               (rs (call-with-new-thread (lambda () (channel-get c)))
+               (rs (list (call-with-new-thread (lambda () (channel-get c))))
                    (cons (call-with-new-thread (lambda () (channel-get c))) rs)))
               ((<= number-of-receivers i) rs)))
          (s0 (call-with-new-thread (lambda () (channel-put c v)))))
     (join-thread s0)
     (= (count thread-exited? receivers) 1)))
 
-(define-test (one-sender-multiple-receivers)
-  (check (sender-then-receivers 42 42)
-         (sender-then-receivers 42 "foo")
-         (receivers-then-sender 42 42)
-         (receivers-then-sender 42 "foo")))
+(test-begin "One Sender, Multiple Receivers")
 
-(define-test (test-channel)
-  (combine-results
-   (simple-tests)
-   (one-to-one)
-   (one-sender-multiple-receivers)))
+(test-assert (sender-then-receivers 42 42))
+(test-assert (sender-then-receivers 42 "foo"))
+(test-assert (receivers-then-sender 42 42))
+(test-assert (receivers-then-sender 42 "foo"))
 
-(define (main args)
-  (test-channel))
+(test-end "One Sender, Multiple Receivers")
