ccf27609c992d50c30f76b307a90bcf09854df7b
Diogo F. S. Ramos <dfsr@riseup.net>
2014-03-28 15:02:04 -0300

    Fix tests
    Test actual returned values, and not how many threads have finished,
    which could fail anyway because of timing issues.

--- test.scm
+++ test.scm
@@ -1,6 +1,16 @@
-(use-modules (srfi srfi-64)
+(use-modules (ice-9 threads)
+             (srfi srfi-64)
              (srfi srfi-1)
              (concurrency channel))
+
+
+;;; utils
+(define* (build-list n proc)
+  (define (build-looping i)
+    (if (<= n i)
+        '()
+        (cons (proc i) (build-looping (+ i 1)))))
+  (build-looping 0))
 
 
 (test-begin "Simple Tests")
@@ -64,26 +74,44 @@
 (test-end "One to One")
 
 (define (sender-then-receivers number-of-receivers v)
-  (let* ((c (make-channel))
-         (s0 (call-with-new-thread (lambda () (channel-put c v))))
-         (receivers
-          (do ((i 0 (1+ i))
-               (rs (list (call-with-new-thread (lambda () (channel-get c))))
-                   (cons (call-with-new-thread (lambda () (channel-get c))) rs)))
-              ((<= number-of-receivers i) rs))))
-    (join-thread s0)
-    (= (count thread-exited? receivers) 1)))
+  (let ((mx (make-mutex))
+        (cv (make-condition-variable)))
+    (with-mutex mx
+      (let* ((c (make-channel))
+             (s0 (call-with-new-thread (lambda () (channel-put c v))))
+             (rv #f)
+             (receivers
+              (build-list number-of-receivers
+                          (lambda (_)
+                            (call-with-new-thread
+                             (lambda ()
+                               (let ((r (channel-get c)))
+                                 (with-mutex mx
+                                   (set! rv r)
+                                   (signal-condition-variable cv)))))))))
+        (join-thread s0)
+        (wait-condition-variable cv mx)
+        (equal? rv v)))))
 
 (define (receivers-then-sender number-of-receivers v)
-  (let* ((c (make-channel))
-         (receivers
-          (do ((i 0 (1+ i))
-               (rs (list (call-with-new-thread (lambda () (channel-get c))))
-                   (cons (call-with-new-thread (lambda () (channel-get c))) rs)))
-              ((<= number-of-receivers i) rs)))
-         (s0 (call-with-new-thread (lambda () (channel-put c v)))))
-    (join-thread s0)
-    (= (count thread-exited? receivers) 1)))
+  (let ((mx (make-mutex))
+        (cv (make-condition-variable)))
+    (with-mutex mx
+      (let* ((c (make-channel))
+             (rv #f)
+             (receivers
+              (build-list number-of-receivers
+                          (lambda (_)
+                            (call-with-new-thread
+                             (lambda ()
+                               (let ((r (channel-get c)))
+                                 (with-mutex mx
+                                   (set! rv r)
+                                   (signal-condition-variable cv))))))))
+             (s0 (call-with-new-thread (lambda () (channel-put c v)))))
+        (join-thread s0)
+        (wait-condition-variable cv mx)
+        (equal? rv v)))))
 
 (test-begin "One Sender, Multiple Receivers")
 
@@ -93,3 +121,50 @@
 (test-assert (receivers-then-sender 42 "foo"))
 
 (test-end "One Sender, Multiple Receivers")
+
+(define (senders-then-receiver number-of-senders v)
+  (let ((mx (make-mutex))
+        (cv (make-condition-variable)))
+    (with-mutex mx
+      (let* ((c (make-channel))
+             (senders (build-list number-of-senders
+                                  (lambda (i)
+                                    (call-with-new-thread
+                                     (lambda ()
+                                       (channel-put c v)
+                                       (with-mutex mx
+                                         (signal-condition-variable cv)))))))
+             (rv #f)
+             (r0 (call-with-new-thread (lambda ()
+                                         (set! rv (channel-get c))))))
+        (join-thread r0)
+        (wait-condition-variable cv mx)
+        (equal? rv v)))))
+
+(define (receiver-then-senders number-of-senders v)
+  (let ((mx (make-mutex))
+        (cv (make-condition-variable)))
+    (with-mutex mx
+      (let* ((c (make-channel))
+             (rv #f)
+             (r0 (call-with-new-thread (lambda ()
+                                         (set! rv (channel-get c)))))
+             (senders (build-list number-of-senders
+                                  (lambda (i)
+                                    (call-with-new-thread
+                                     (lambda ()
+                                       (channel-put c v)
+                                       (with-mutex mx
+                                         (signal-condition-variable cv))))))))
+        (join-thread r0)
+        (wait-condition-variable cv mx)
+        (equal? rv v)))))
+
+(test-begin "Multiple Senders, Single receiver")
+
+(test-assert (senders-then-receiver 42 42))
+(test-assert (senders-then-receiver 42 "foo"))
+(test-assert (receiver-then-senders 42 42))
+(test-assert (receiver-then-senders 42 "foo"))
+
+(test-end "Multiple Senders, Single receiver")
